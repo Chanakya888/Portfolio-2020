0048d0f2b1646563aeab5c61cae8907f92ecc6a5
Chanakya Kilaru <samuraichanakya1@gmail.com>
2020-11-19 11:05:36 +0530

    removing smooth scroll and trying out noise background

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,58 +15,58 @@ export default function Home() {
 	const app = useRef();
 	const scrollContainer = useRef();
 
-	// Configs
-	const data = {
-		ease: 0.1,
-		current: 0,
-		previous: 0,
-		rounded: 0
-	};
+	// // Configs
+	// const data = {
+	// 	ease: 0.1,
+	// 	current: 0,
+	// 	previous: 0,
+	// 	rounded: 0
+	// };
 
-	// Running scrollrender once page is loaded.
-	useEffect(() => {
-		requestAnimationFrame(() => skewScrolling());
-	});
+	// // Running scrollrender once page is loaded.
+	// useEffect(() => {
+	// 	requestAnimationFrame(() => skewScrolling());
+	// });
 
-	//set the height of the body.
-	useEffect(
-		() => {
-			setBodyHeight();
-		},
-		[ size.height ]
-	);
+	// //set the height of the body.
+	// useEffect(
+	// 	() => {
+	// 		setBodyHeight();
+	// 	},
+	// 	[ size.height ]
+	// );
 
-	//Set the height of the body to the height of the scrolling div
-	const setBodyHeight = () => {
-		document.body.style.height = `${scrollContainer.current.getBoundingClientRect().height}px`;
-	};
+	// //Set the height of the body to the height of the scrolling div
+	// const setBodyHeight = () => {
+	// 	document.body.style.height = `${scrollContainer.current.getBoundingClientRect().height}px`;
+	// };
 
-	// Scrolling
-	const skewScrolling = () => {
-		if (scrollContainer.current !== null) {
-			//Set Current to the scroll position amount
-			data.current = window.scrollY;
-			// Set Previous to the scroll previous position
-			data.previous += (data.current - data.previous) * data.ease;
-			// Set rounded to
-			data.rounded = Math.round(data.previous * 100) / 100;
+	// // Scrolling
+	// const skewScrolling = () => {
+	// 	if (scrollContainer.current !== null) {
+	// 		//Set Current to the scroll position amount
+	// 		data.current = window.scrollY;
+	// 		// Set Previous to the scroll previous position
+	// 		data.previous += (data.current - data.previous) * data.ease;
+	// 		// Set rounded to
+	// 		data.rounded = Math.round(data.previous * 100) / 100;
 
-			// Difference between
-			// const difference = data.current - data.rounded;
-			// const acceleration = difference / size.width;
-			// const velocity = +acceleration;
-			// const skew = velocity * 7.5
+	// 		// Difference between
+	// 		// const difference = data.current - data.rounded;
+	// 		// const acceleration = difference / size.width;
+	// 		// const velocity = +acceleration;
+	// 		// const skew = velocity * 7.5
 
-			//Assign skew and smooth scrolling to the scroll container
-			scrollContainer.current.style.transform = `translate3d(0, -${data.rounded}px, 0)`;
+	// 		//Assign skew and smooth scrolling to the scroll container
+	// 		scrollContainer.current.style.transform = `translate3d(0, -${data.rounded}px, 0)`;
 
-			//loop vai raf
-			requestAnimationFrame(() => skewScrolling());
-		}
-	};
-	console.log('scroll continaer', scrollContainer.current);
+	// 		//loop vai raf
+	// 		requestAnimationFrame(() => skewScrolling());
+	// 	}
+	// };
+	// console.log('scroll continaer', scrollContainer.current);
 	return (
-		<div ref={app} className="App">
+		<div>
 			<div ref={scrollContainer} className="scroll">
 				<div className="background">
 					<div className="components-foreground">
