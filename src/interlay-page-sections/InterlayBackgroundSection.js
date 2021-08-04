7ef1a5a34dee7d98490cb8f29628a7bfd97d09b7
Chanakya Kilaru <samuraichanakya1@gmail.com>
2021-08-04 23:34:29 +0530

    fix: typo

--- src/interlay-page-sections/InterlayBackgroundSection.js
+++ src/interlay-page-sections/InterlayBackgroundSection.js
@@ -10,8 +10,10 @@ const InterlayBackgroundSection = () => {
 	A 1:1 Bitcoin-backed asset, fully collateralized, interoperable, and censorship-resistant. 
 	I worked for Interlay from January 2021 to July 2021 as a UI/UX designer and a frontend developer.Being a startup with around 10 developers, 
 	I had a mixed bag of responsibilities. I started with working on a deadline where I designed the polkaBTC(now interBTC) app in less than two weeks right in time for beta launch. Later, 
-	when we transitioned to interBTC, I built a design system in Figma and converted it into code using react and storybook components. I’ve also worked on developing user flows, wireframes 
-	and redesigned UI of the application with new features like Vault staking and Vault activity. I've also worked on developing interlay's website and several interBTC app features. </p>
+	when we transitioned to interBTC, I built a design system in Figma and converted it into code using react and storybook components. 
+  Later, I worked on developing user flows, wireframes, redesigned UI of the application with new features 
+  like Vault staking and Vault activity and developed several interBTC app features. 
+  I've also offered support to users using interBTC app on Interlay's discord</p>
 	<p>The biggest challenge that I faced when starting out with the Interlay is the complexity of interBTC. 
 	Being a decentralised system having multiple system maintainers which results in a lot of edge cases, it was essential to abstract the complex mechanism and provide the user with a seamless experience of Bitcoin Defi.
 	</p>
